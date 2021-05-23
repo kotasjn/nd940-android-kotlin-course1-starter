3133fabedbce78da26fae5932f3009f4506eaae3
Jan Kotas <kotasjn@gmail.com>
2021-05-23 22:19:41 +0200

    Add welcome screen

--- app/src/main/java/com/udacity/shoestore/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/LoginFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.udacity.shoestore.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -18,9 +19,18 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        val view = binding.root
 
         (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.login_title)
 
-        return binding.root
+        binding.loginButton.setOnClickListener { onButtonPressed(view) }
+        binding.registerButton.setOnClickListener { onButtonPressed(view) }
+
+        return view
+    }
+
+    private fun onButtonPressed(view: View) {
+        val action = LoginFragmentDirections.actionLoginFragmentToWelcomeFragment()
+        view.findNavController().navigate(action)
     }
 }
