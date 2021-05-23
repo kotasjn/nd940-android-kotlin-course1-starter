149e14c2a9c42e71b44354d1a1ca5a2ffc96a20b
Jan Kotas <kotasjn@gmail.com>
2021-05-23 19:15:44 +0200

    Moving Android project into main directory, add Navigation and SafeArgs dependency, create Login screen

---
+++ app/src/main/java/com/udacity/shoestore/LoginFragment.kt
@@ -0,0 +1,26 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+
+        (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.login_title)
+
+        return binding.root
+    }
+}
