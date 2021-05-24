33e8f367f09749891dfebd7fe6ef82320fe39dc7
Jan Kotas <kotas@sonet.cz>
2021-05-24 20:38:37 +0200

    Add ShoeViewModel

---
+++ app/src/main/java/com/udacity/shoestore/ShoeViewModel.kt
@@ -0,0 +1,24 @@
+package com.udacity.shoestore
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.udacity.shoestore.models.Shoe
+
+class ShoeViewModel : ViewModel() {
+
+    private val _shoeList = MutableLiveData<List<Shoe>>()
+    val shoeList: LiveData<List<Shoe>>
+        get() = _shoeList
+
+    private val shoes = listOf(
+        Shoe("Asics Gel Nimbus 23", 42.0, "Asics", "Great design, perfect feeling"),
+        Shoe("Hoka One Rocket X", 45.0, "Hoka", "Extremely lightweight shoes"),
+        Shoe("Brooks Levitate 4", 40.0, "Brooks", "Very durable shoes")
+    )
+
+    init {
+        _shoeList.value = shoes
+    }
+
+}
